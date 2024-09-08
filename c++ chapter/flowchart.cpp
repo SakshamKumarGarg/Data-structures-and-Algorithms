eae7db842cdb8d8f5a84f7b8525f202d0f91dff9
Saksham <sakshamgarg11jee@gmail.com>
2024-09-08 18:54:20 +0530

    New changes

--- c++ chapter/flowchart.cpp
+++ c++ chapter/flowchart.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main(){
-    int a = 4;
-    cout<<a/2<<endl;
+    // int a = 4;
+    // cout<<a/2<<endl;
+
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    for (int i = n; i > 0; i--)
+    {
+        cout<<i<<" ";
+        /* code */
+    }
+    
 return 0;
 
 }
