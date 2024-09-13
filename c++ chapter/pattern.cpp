56c5c9387c5c3a6d58f5288baeac6ebe8407f080
Saksham <sakshamgarg11jee@gmail.com>
2024-09-13 22:31:15 +0530

    new changes

--- c++ chapter/pattern.cpp
+++ c++ chapter/pattern.cpp
@@ -2,17 +2,56 @@
 using namespace std;
 
 int main(){
+    
 
-    for (int row = 0; row < 4; row++)
-    {
-        for (int col = 0; col < 4; col++)
-        {
-            cout<<"*";
-            /* code */
-        }
-        cout<<endl;
-        /* code */
-    }
+    // int length,width;
+    // cout<<"enter length:";
+    // cin>>length;
+
+    // cout<<"Enter width:";
+    // cin>>width;
+
+    // for (int r = 0; r < length; r++)
+    // {
+    //     for (int c = 0; c < width; c++)
+    //     {
+    //         cout<<" * ";
+    //         /* code */
+    //     }
+    //     cout<<endl;
+        
+    //     /* code */
+    // }
+    
+
+
+    // int side;
+    // cout<<"Enter value of size of square:";
+    // cin>>side;
+
+    // for (int r = 0; r <= side; r++)
+    // {
+    //     for (int c = 0; c < side; c++)
+    //     {
+    //         cout<<" * ";
+    //         /* code */
+    //     }
+    //     cout<<endl;
+        
+    //     /* code */
+    // }
+    
+
+    // for (int row = 0; row < 4; row++)
+    // {
+    //     for (int col = 0; col < 4; col++)
+    //     {
+    //         cout<<"*";
+    //         /* code */
+    //     }
+    //     cout<<endl;
+    //     /* code */
+    // }
     
 return 0;
 
