9bb4a0aa91fcbac69f8bb3a6b220ac5df7dc96a1
Saksham <sakshamgarg11jee@gmail.com>
2024-09-13 22:02:26 +0530

    New changes

---
+++ c++ chapter/pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for (int row = 0; row < 4; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            cout<<"*";
+            /* code */
+        }
+        cout<<endl;
+        /* code */
+    }
+    
+return 0;
+
+}
