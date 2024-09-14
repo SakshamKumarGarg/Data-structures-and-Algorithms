df6d45dfe7dd04e1e18b191fb2ce02d707432689
Saksham <sakshamgarg11jee@gmail.com>
2024-09-14 17:16:37 +0530

    New changes

--- c++ chapter/pattern.cpp
+++ c++ chapter/pattern.cpp
@@ -3,31 +3,179 @@ using namespace std;
 
 int main(){
 
-    //HOLLOW SQUARE
-    for (int r = 0; r < 5; r++)
+    //Numeric inverted half pyramid
+    for (int r = 0; r < 4; r++)               // ANOTHER METHOD
     {
-        for (int c = 0; c < 5; c++)
+        for (int c = 0; c < (4-r); c++)
         {
-            if (r==0||r==4)
-            {
-                cout<<" * ";
-                /* code */
-            }
-            else if (c==0||c==4)
-            {
-                cout<<" * ";
-                /* code */
-            }
-            else{
-                cout<<"   ";
-            }
-            
-            
+            cout<<c+1<<" ";
             /* code */
         }
         cout<<endl;
-                /* code */
+        
+        /* code */
     }
+
+    //NUMERIC HALF PYRAMID
+    // for (int r = 0; r < 5; r++)
+    // {
+    //     for (int c = 0; c < r+1; c++)  // we can also put c<=r than c < r+1
+    //     {
+    //         cout<<(c+1)<<" ";
+    //         /* code */
+    //     }
+    //     cout<<endl;
+        
+    //     /* code */
+    // }
+
+
+    //INVERTED HOLLOW HALF PYRAMID PATTERN
+    // for (int r = 0; r < 6; r++)
+    // {
+    //     for (int c = 0; c < (6-r); c++)
+    //     {
+    //         if (r==0||r==4||r==5)
+    //         {
+    //             cout<<" * ";
+    //             /* code */
+    //         }
+    //         else if (c==((6-r)-1)||c==0)
+    //         {
+    //             cout<<" * ";
+    //             /* code */
+    //         }
+            
+    //         else
+    //         {
+    //             cout<<"   ";
+    //             /* code */
+    //         }
+    //         /* code */
+    //     }
+    //     cout<<endl;
+        
+    //     /* code */
+    // }
+
+    //HOLLOW HALF PYRAMID PATTERN
+    // for (int r = 0; r < 6; r++)
+    // {
+    //     for (int c = 0; c < r+1; c++)
+    //     {
+    //         if (r==0||r==1||r==5)
+    //         {
+    //             cout<<" * ";
+    //             /* code */
+    //         }
+    //         else if (c==r||c==0)
+    //         {
+    //             cout<<" * ";
+    //             /* code */
+    //         }
+            
+    //         else
+    //         {
+    //             cout<<"   ";
+    //             /* code */
+    //         }
+    //         /* code */
+    //     }
+    //     cout<<endl;
+        
+    //     /* code */
+    // }
+
+    // for (int r = 0; r < 6; r++)              //ANOTHER METHOD
+    // {
+    //     for (int c = 0; c < r+1; c++)
+    //     {
+    //         if (r==0||r==1||r==5||c==0||c==r)
+    //         {
+    //             cout<<" * ";
+    //             /* code */
+    //         }
+    //         else{
+    //             cout<<"   ";
+    //         }
+            
+    //         /* code */
+    //     }
+    //     cout<<endl;
+        
+    //     /* code */
+    // }
+    
+    
+
+    //INVERTED HALF PYRAMID
+    // for (int r = 3; r >= 0; r--)
+    // {
+    //     for (int c = r; c>=0; c--)
+    //     {
+    //         cout<<" * ";
+    //         /* code */
+    //     }
+    //     cout<<endl;
+        
+    //     /* code */
+    // }
+
+    // for (int r = 0; r < 4; r++)               // ANOTHER METHOD
+    // {
+    //     for (int c = 0; c < (4-r); c++)
+    //     {
+    //         cout<<" * ";
+    //         /* code */
+    //     }
+    //     cout<<endl;
+        
+    //     /* code */
+    // }
+    
+    
+
+
+
+    //Half Pyramid Pattern
+    // for (int r = 0; r < 5; r++)
+    // {
+    //     for (int c = 0; c < r+1; c++)  // we can also put c<=r than c < r+1
+    //     {
+    //         cout<<" * ";
+    //         /* code */
+    //     }
+    //     cout<<endl;
+        
+    //     /* code */
+    // }
+    
+
+    //HOLLOW SQUARE
+    // for (int r = 0; r < 5; r++)
+    // {
+    //     for (int c = 0; c < 5; c++)
+    //     {
+    //         if (r==0||r==4)
+    //         {
+    //             cout<<" * ";
+    //             /* code */
+    //         }
+    //         else if (c==0||c==4)
+    //         {
+    //             cout<<" * ";
+    //             /* code */
+    //         }
+    //         else{
+    //             cout<<"   ";
+    //         }
+            
+            
+    //         /* code */
+    //     }
+    //     cout<<endl;
+    //             /* code */
+    // }
     
 
     // HOLLOW RECTANGLE PATTEREN
