8ea3332ae9e3967672c246277592a6fc70cd04ae
Saksham <sakshamgarg11jee@gmail.com>
2024-10-11 22:09:48 +0530

    new changes

--- c++ chapter/debug2.cpp
+++ c++ chapter/debug2.cpp
@@ -3,31 +3,123 @@ using namespace std;
 
 
     //Debug the code. Return true if the number is member of fibonacci series else false.
-int checkMember(int n){
-    int f1 = 1, f2 = 1;
-    if(n==0 || n==1){
-        return true;
-    }
-    else{
-        while(1){
-            int temp = f1 + f2;
-            f1 = f2;
-            f2 = temp;
-            if(temp==n){
-                return true;
-            }
-            else if(temp>n){
-                return false;
-            }
-        }
-    }
+// int checkMember(int n){
+//     int f1 = 1, f2 = 1;
+//     if(n==0 || n==1){                   // && is wrong here we should use || operator
+//         return true;
+//     }
+//     else{
+//         while(1){
+//             int temp = f1 + f2;
+//             f1 = f2;
+//             f2 = temp;
+//             if(temp==n){
+//                 return true;
+//             }
+//             else if(temp>n){
+//                 return false;
+//             }
+//         }
+//     }
 
-}
+// }
 
 int main(){
+    //Debug the code. Correct the code for printing the pattern.
+    /*
+Pattern for N = 5
+1234554321
+1234**4321
+123****321
+12******21
+1********1
+*/
 
-    int ans = checkMember(1);
-    cout<<ans<<endl;
+    int n;
+    cin>>n;
+    int i=0,gaps = n-1;
+    while(i<n){
+        int k=0,gaps=n-1-i;
+        while(k<gaps){
+            cout<<" ";
+            k++;
+        }
+
+        int num = i+1;
+        while(num>0){
+            cout<<num;
+            num = num - 1;
+        }
+        k = 2;
+        while(k<=(i+1)){
+            cout<<k;
+            k++;
+        }
+        k =0;
+        while(k<gaps){
+            cout<<" ";
+            k++;
+        }
+        i++;
+        cout<<"\n";
+    }
+
+    //Debug the code. Correct the code for printing the pattern.
+    /*
+Pattern for N = 4
+   1
+  212
+ 32123
+4321234
+*/
+
+    // int n;
+    // cin>>n;
+    // int i=0,gaps = n-1;
+    // while(i<n){
+    //     int k=0,gaps=n-1-i;
+    //     while(k<gaps){
+    //         cout<<" ";
+    //         k++;
+    //     }
+    //     int num = i+1;
+    //     while(num>0){
+    //         cout<<num;
+    //         num = num - 1;
+    //     }
+    //     k = 2;
+    //     while(k<=(i+1)){
+    //         cout<<k;
+    //         k++;
+    //     }
+    //     k =0;
+    //     while(k<gaps){
+    //         cout<<" ";
+    //         k++;
+    //     }
+    //     i++;
+    //     cout<<"\n";
+    // }
+
+    //Debug the code to print the following pattern
+
+/*
+Pattern for N = 4
+4444
+333
+22
+1
+*/
+//   int i,j,n;
+//     cin>>n;
+//     for(i=1;i<=n;i++){
+//         for(j=i;j<=n;j++){
+//             cout<<(n-i+1);
+//         }
+//         cout<<endl;
+//     }
+    // int ans = checkMember(1);
+    // cout<<ans<<endl;
    
 
        //Debug the code. Print all the prime numbers that lie in the range 2 to N (both inclusive)
