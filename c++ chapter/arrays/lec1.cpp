f8e24a7507532c4ee969ebf54403191677bfa0eb
Saksham <sakshamgarg11jee@gmail.com>
2024-10-17 22:52:08 +0530

    new changes

---
+++ c++ chapter/arrays/lec1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[3];
+    fill(arr,arr+3,24);
+    cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<endl;
+    // int arr[4];
+    // int brr[] = {1,2,3,4};
+    // int drr[4] = {5};
+
+    // cout<<drr[2]<<endl;
+
+    // cout<<drr[0]<<endl;
+    // cout<<brr[1]<<endl;
+    // cout<<brr[2]<<endl;
+    // cout<<brr[5]<<endl;
+
+    // int cr[6] = {23,45,64};
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     cr[i] = {23};
+    //     /* code */
+    // }
+    // cout<<" "<<cr[0]<<" "<<cr[1]<<" "<<cr[2]<<" "<<cr[3]<<" "<<cr[4]<<" "<<cr[5]<<endl;
+    
+return 0;
+
+}
