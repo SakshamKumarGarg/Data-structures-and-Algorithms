76d49ace305cce8fb0a029cc8ba937b1b4d4c847
Saksham <sakshamgarg11jee@gmail.com>
2024-11-19 16:11:29 +0530

    new changes

---
+++ c++ chapter/basic of c++/week2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for (int r = 0; r < n; r++)
+    {
+        for (int c = 0; c < r+1; c++)
+        {
+            if(c==)
+            cout<<c+1;
+
+            /* code */
+        }
+        cout<<endl;
+        
+        /* code */
+    }
+    
+
+return 0;
+}
