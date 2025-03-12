397bb8dc8e509eeb91cb57ed855bd258aaad10f7
Saksham <sakshamgarg11jee@gmail.com>
2025-03-12 21:28:05 +0530

    new changes

---
+++ c++ chapter/stacks/prev_smaller_element.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(stack<int> &s, vector<int> &arr,vector<int> &ans){
+    int n = arr.size();
+    for(int i=0;i<n;i++){
+        int element = arr[i];
+        while (s.top()>=element)
+        {
+            s.pop();
+        }
+
+        ans.push_back(s.top());
+        s.push(element);
+        
+    }
+}
+int main(){
+    vector<int> v;
+    v.push_back(8);
+    v.push_back(4);
+    v.push_back(6);
+    v.push_back(2);
+    v.push_back(3);
+
+    stack<int> s;
+    s.push(-1);
+
+    vector<int> ans;
+    solve(s,v,ans);
+
+    // reverse(ans.begin(),ans.end());
+
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+
+    return 0;
+
+}
