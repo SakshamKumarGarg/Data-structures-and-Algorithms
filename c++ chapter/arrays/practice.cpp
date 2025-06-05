4d140aec468c1668c5caf42927a6dd6c801f1f11
Saksham Garg <youremail@example.com>
2025-06-05 16:17:00 +0000

    new changes

---
+++ c++ chapter/arrays/practice.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int main(){
+	vector<int> a;
+	for(int i=0;i<10;i++){
+		a.push_back(i);
+	}
+
+	cout<<a[1]<<" "<<a[2]<<" "<<a[6]<<" "<<a[9];
+	return 0;
+}
